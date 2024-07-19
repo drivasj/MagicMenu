eb40b2ef5b434c663305c9dcfce9e2daa870f749
Cuts03 <danielrvs18@gmail.com>
2024-07-19 16:14:29 -0400

    CategoryDash Part 1

---
+++ DigitalMenu/Controllers/CategoryDisheController.cs
@@ -0,0 +1,23 @@
+﻿using DigitalMenu.Models.CategoryDishe;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalMenu.Controllers
+{
+    [Route("api/Categorydishe")]
+    [ApiController]
+    public class CategoryDisheController : ControllerBase
+    {
+        public CategoryDisheController()
+        {
+            
+        }
+
+        //[HttpGet]
+        //public async Task<ActionResult<CategoryDashDTO>> get()
+        //{
+        //    var categorys = await 
+        //}
+
+    }
+}
