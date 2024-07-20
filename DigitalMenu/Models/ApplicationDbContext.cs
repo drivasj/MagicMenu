3ddb84ad49784fc9163597f3e98114c219ac60c5
Cuts03 <danielrvs18@gmail.com>
2024-07-20 15:32:13 -0400

    Add project files.

---
+++ DigitalMenu/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using DigitalMenu.Models;
+
+namespace TestWeb
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
