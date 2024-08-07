8544394ed05f760ae124369bc5c93c3f7a28a5cd
Cuts03 <danielrvs18@gmail.com>
2024-08-07 15:50:15 -0400

    NavbarComponent

---
+++ DigitalMenu/Components/Navbar/NavbarViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalMenu.Components.Navbar
+{
+    public class NavbarViewComponent : ViewComponent
+    {
+        public NavbarViewComponent()
+        {           
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            int idUser = 1;
+            return View(idUser);
+        }
+    }
+}
